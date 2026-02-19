9036c5dcf79cc5a86f4b7f1705a008517cecce5f
Arya Adhi Prasetyo <ardhityo229@gmail.com>
2026-02-19 15:07:12 +0700

    Multi-Page Application

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import {defineConfig} from "vite";
 export default defineConfig({
   build: {
     outDir: "production",
+    rollupOptions: {
+      /**
+       * Secara default jika tidak punya vite.config.js maka ketika build hanya index.html saja
+       * Jika buat config vite.config.js dan buat input build selain index.html maka akan di build juga
+       * Build : npx vite build
+       * Run vite : npx vite
+       * Run vite preview : npx vite preview
+       */
+      input: {
+        index: "index.html",
+        blog: "blog.html",
+        contact: "other/contact.html",
+      },
+    },
   },
   server: {
     port: 3000,
